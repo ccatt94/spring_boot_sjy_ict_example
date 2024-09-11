f5fb7721a5106b3d097d36b706f98564b9e63f2a
WIN-G8EHF2IUQB8\ICT02_19 <sally940828@naver.com>
2024-09-11 11:36:04 +0900

    commit

---
+++ src/main/java/edu/ict/ex/SpringBootSjyIctExampleApplication.java
@@ -0,0 +1,13 @@
+package edu.ict.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSjyIctExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSjyIctExampleApplication.class, args);
+	}
+
+}
